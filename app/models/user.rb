944b9ed030cb63e9f0cf445eb47d6a3eb01a7523
Nigel Blackmore <nigel@blackmore.de>
2012-03-06 10:55:20 +0100

    Relationship user to native language resolved

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   set_table_name "Staff"
+  set_primary_key "StaffID"
+  alias_attribute :group_id, 'GroupID'
+  alias_attribute :login, 'Login'
   alias_attribute :name, 'Name'
-  
+  alias_attribute :password, 'Password'
+  alias_attribute :active, 'Active'
+  alias_attribute :nativelanguage_id, 'NativeLanguageID'
+  alias_attribute :lastsaved_id, 'LastSavedID'
+  alias_attribute :updated_at, 'LastChanged'
+  belongs_to :nativelanguage, :class_name => "Language", :foreign_key => "NativeLanguageID", :primary_key => "LanguageID"
 end
